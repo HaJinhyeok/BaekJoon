1428089f0f1352ae043e9f8bc2f8e4373f94ff05
HaJinhyeok <hjhmoon61@naver.com>
2025-08-01 11:51:31 +0900

    2580_스도쿠

--- 2580_스도쿠.cpp
+++ 2580_스도쿠.cpp
@@ -7,6 +7,40 @@ using namespace std;
 // 처음 입력받을 때 0인 위치 저장할 벡터 하나,
 // priority_queue 하나?
 
+// (x,y) 위치를 기준으로 스도쿠 조건 만족하는지 검사
+bool IsPossible(int** board, int x, int y)
+{
+	int sum = 0;
+	bool isChecking = true;
+	// 가로
+	for (int i = 0; i < 9; i++)
+	{
+		// 아직 빈칸이 있으면 일단 보류. 한 줄이 다 채워져야 확인되기 때문
+		if (board[x][i] == 0)
+		{
+			isChecking = false;
+			break;
+		}
+		sum += board[x][i];
+	}
+	if (isChecking && sum != 45)
+		return false;
+	// 세로
+	sum = 0;
+	isChecking = true;
+	for (int i = 0; i < 9; i++)
+	{
+		if (board[i][y] == 0)
+		{
+			isChecking = false;
+			break;
+		}
+		sum += board[i][y];
+	}
+	if (sum != 45)
+		return false;
+	// 블록 단위
+}
 
 int main()
 {
