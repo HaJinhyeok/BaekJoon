25848557580a7ae0d386ce5a4eef700c9f452c13
HaJinhyeok <hjhmoon61@naver.com>
2025-08-13 13:24:29 +0900

    2580_스도쿠 일단 해결

--- 2580_스도쿠.cpp
+++ 2580_스도쿠.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <iterator>
 
 using namespace std;
 
@@ -38,68 +36,43 @@ bool Backtracking(int idx)
 	int x = idx / n;
 	int y = idx % n;
 	if (sudoku[x][y] > 0)
-		return true;
-}
-
-// (x,y) 위치를 기준으로 스도쿠 조건 만족하는지 검사
-bool IsPossible(int** board, int x, int y)
-{
-	int sum = 0;
-	bool isChecking = true;
-	// 가로
-	for (int i = 0; i < 9; i++)
+		return Backtracking(idx + 1);
+	for (int i = 1; i <= 9; i++)
 	{
-		// 아직 빈칸이 있으면 일단 보류. 한 줄이 다 채워져야 확인되기 때문
-		if (board[x][i] == 0)
+		if (!row[x][i] && !column[y][i] && !block[GetBlockNum(x, y)][i])
 		{
-			isChecking = false;
-			break;
+			sudoku[x][y] = i;
+			row[x][i] = true;
+			column[y][i] = true;
+			block[GetBlockNum(x, y)][i] = true;
+			if (Backtracking(idx + 1))
+			{
+				return true;
+			}
+			sudoku[x][y] = 0;
+			row[x][i] = false;
+			column[y][i] = false;
+			block[GetBlockNum(x, y)][i] = false;
 		}
-		sum += board[x][i];
 	}
-	if (isChecking && sum != 45)
-		return false;
-	// 세로
-	sum = 0;
-	isChecking = true;
-	for (int i = 0; i < 9; i++)
-	{
-		if (board[i][y] == 0)
-		{
-			isChecking = false;
-			break;
-		}
-		sum += board[i][y];
-	}
-	if (sum != 45)
-		return false;
-	// 블록 단위
+	return false;
 }
 
 int main()
 {
-	int sudokuBoard[9][9];
-	vector<pair<int, int>> zeroList;
+	int enter;
 
 	for (int i = 0; i < 9; i++)
 	{
 		for (int j = 0; j < 9; j++)
 		{
-			cin >> sudokuBoard[i][j];
-			// 값이 0인 칸의 위치, 본인 포함 가로 및 세로에 0이 몇 칸, 3 by 3 matrix에 0 몇 칸인지 저장
-			if (sudokuBoard[i][j] == 0)
-			{
-				zeroList.emplace_back(make_pair(i, j));
-			}
+			cin >> enter;
+				sudoku[i][j]=enter;
+				row[i][enter] = true;
+				column[j][enter] = true;
+				block[GetBlockNum(i, j)][enter] = true;
 		}
 	}
-
-	// 결과 출력
-	for (int i = 0; i < 9; i++)
-	{
-		for (int j = 0; j < 9; j++)
-			cout << sudokuBoard[i][j] << ' ';
-		cout << '\n';
-	}
+	Backtracking(0);
 	return 0;
 }
